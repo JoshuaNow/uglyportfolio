29a952031c4b6eb3225b3d6528b21812ab2614fd
Joshua Lopez <jrenelopez92@gmail.com>
2020-12-08 16:29:57 -0600

    finshed

---
+++ index.js
@@ -0,0 +1,41 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+const server = http.createServer(app);
+
+const es6Renderer = require("express-es6-template-engine");
+app.engine("html", es6Renderer);
+app.set("views", "templates");
+app.set("view engine", "html");
+
+const projects = [
+  "python_project",
+  "front_end",
+  "back_end",
+  "react",
+  "capstone",
+];
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/projects", (req, res) => {
+  const names = projects;
+  res.render("projects", {
+    locals: {
+      names,
+    },
+  });
+});
+
+app.get("/projects/:id", (req, res) => {
+  //  this will work for all the links i just need to make a html for every id
+  const { id } = req.params;
+  res.render(id);
+  //   res.render("description");
+});
+
+server.listen(3000, "localhost", () => {
+  console.log("running on 3000");
+});

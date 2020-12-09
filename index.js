e32708125ed293f9133cc1f4a7243ed074a42123
Joshua Lopez <jrenelopez92@gmail.com>
2020-12-09 10:27:47 -0600

    added better function

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ app.set("view engine", "html");
 
 const names = ["python_project", "front_end", "back_end", "react", "capstone"];
 
+const objprojects = {
+  BankVault: "Game to crack a safe or go to jail",
+  TikiHut: "Great place for advice",
+  RedClover: "The Great Creation in the Making",
+  CorruptionSolution: "Solving Greed around the World",
+  GenerationBank: "A New Bank, For A New Generation",
+};
+
 app.get("/", (req, res) => {
   res.render("home");
 });
@@ -18,16 +26,24 @@ app.get("/projects", (req, res) => {
   //   const names = projects;
   res.render("projects", {
     locals: {
-      names,
+      names, //function 1
+      names: Object.keys(objprojects), // function 2
     },
   });
 });
-
+// function 1
+// // this is for to go through each id but we have to make a website for each key
+// app.get("/projects/:id", (req, res) => {
+//   //  this will work for all the links i just need to make a html for every id
+//   const { id } = req.params;
+//   res.render(id);
+//   //   res.render("description");
+// });
+//function 2
+// this is so i just need to use one website
 app.get("/projects/:id", (req, res) => {
-  //  this will work for all the links i just need to make a html for every id
   const { id } = req.params;
-  res.render(id);
-  //   res.render("description");
+  res.render("description", { locals: { id, des: objprojects[id] } });
 });
 
 server.listen(3000, "localhost", () => {
